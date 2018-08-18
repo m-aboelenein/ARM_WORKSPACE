9cec311cfa2e5a19a4805b4d8b73d45a4e368f0e
D <D@DESKTOP-F43S772.lan>
2018-08-18 02:07:24 +0200

    second commit

--- Blink_PatrickHood/src/main.c
+++ Blink_PatrickHood/src/main.c
@@ -2,32 +2,52 @@
 
 #include "stm32f4xx.h"
 
-// Create a function to wait for a specific amount of time
+//function to Delay for a specific amount of time
+void DELAY(int delay_time)
+{
+
+	volatile int i,j ;
+
+	for(i=0;i<delay_time;i++) {
+		j++;
+	}
+}
+
+
 
 int main(void)
 {
 	// Enable the GPIO clock for port A using RCC and AHB
-    RCC ->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
+	RCC ->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
     // or RCC ->AHB1ENR |= (1<<19)
 
-	// set any registers to control port A pin 5
+	/*
+	 *  set any registers to control port A pin 5
+	 *  x    11
+	 *  x_0  01
+	 *  x_1  10
 
+   */
     GPIOA ->MODER |= GPIO_MODER_MODER5_0 ; // OUTPUT MODE
+    GPIOA ->MODER &= ~(GPIO_MODER_MODER5_1) ;
+    /* to ensure the left bit is a zero and not affected by another operation
+       so the OR can work Correctly
+    */
     GPIOA ->OTYPER &=~(GPIO_OTYPER_OT_5); // PUSH PULL MODE
     GPIOA ->OSPEEDR |= GPIO_OSPEEDER_OSPEEDR5; //HIGH SPEED MODE
-	GPIOA ->PUPDR &=~ (GPIO_PUPDR_PUPDR5);
+    GPIOA ->PUPDR &=~ (GPIO_PUPDR_PUPDR5);  // None (NOPUPD)
 
 
-  int i = 0;
 
-
-  while (1){
-	  //turn on LED using BRR
+  while (1)
+  {
+	  //turn on LED using BSRR
+	  GPIOA->BSRRL |= GPIO_BSRR_BS_5;
 	  //wait
-	  //turn off lED using BRR
-
-
-	i++;
+	  DELAY(200000);
+	  //turn off lED using BSRR
+	  GPIOA->BSRRH |= GPIO_BSRR_BR_5;
+	  DELAY(200000);
 
   }
 }
