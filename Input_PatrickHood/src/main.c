a8da286269c615d2258b8ab6a232cb29f547a6ef
D <D@DESKTOP-F43S772.lan>
2018-08-18 16:30:08 +0200

    Final Commit

--- Input_PatrickHood/src/main.c
+++ Input_PatrickHood/src/main.c
@@ -15,24 +15,24 @@ int main(void)
        so the OR can work Correctly
     */
     GPIOA ->OTYPER &= ~(GPIO_OTYPER_OT_5); // PUSH PULL MODE
-    GPIOA ->OSPEEDR |= GPIO_OSPEEDER_OSPEEDR5; //HIGH SPEED MODE
+    GPIOA ->OSPEEDR |= GPIO_OSPEEDER_OSPEEDR5_0 | GPIO_OSPEEDER_OSPEEDR5_1 ; //HIGH SPEED MODE
     GPIOA ->PUPDR &= ~(GPIO_PUPDR_PUPDR5);  // None (NOPUPD)
 
     // Setting The Control Registers for the Switch
     GPIOC ->MODER &= ~(GPIO_MODER_MODER13);  //INPUT MODE
-    GPIOA ->PUPDR &= ~(GPIO_PUPDR_PUPDR13);  // None (NOPUPD)
+    GPIOC ->PUPDR &= ~(GPIO_PUPDR_PUPDR13);  // None (NOPUPD)
 
 
 
   while (1)
   {
-	 if (GPIOB ->IDR & GPIO_IDR_IDR_13 )
+	 if (GPIOC ->IDR & GPIO_IDR_IDR_13 )
 	 {
-		 GPIOA->BSRRL |= (1<<5);
+		 GPIOA->BSRRH |= (1<<5); // Set Bit 5
 	 }
 	 else {
-		  GPIOA->BSRRH |= (1<<5);
 
+		  GPIOA->BSRRL |= (1<<5); // Reset Bit 5
 	 }
 
 
