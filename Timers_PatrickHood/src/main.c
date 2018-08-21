f85c29d5b38c33ab3556f331d49c2cb2c3551592
DTM <DTM@DESKTOP-F43S772>
2018-08-21 03:43:59 +0200

    Timer Basics

--- Timers_PatrickHood/src/main.c
+++ Timers_PatrickHood/src/main.c
@@ -5,7 +5,11 @@
 int main(void)
 {
 
-
+	RCC ->APB2ENR |= RCC_APB2ENR_TIM1EN; // Enable Clock for TIMER 1
+    TIM1 -> PSC =10000;  // SET PRESCALLER TO 10000
+    TIM1 -> CR1 |= TIM_CR1_CEN; // Enable Timer 1
+    TIM1 -> CR1 |= TIM_CR1_DIR; // Set the Direction to Down Counter
+    TIM1 -> ARR = 1000;   // Set overflow register to 1000
 
 
   while (1)
